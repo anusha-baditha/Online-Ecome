a41991a95ef351fe2179ad666aa93793e6b34f97
anusha-baditha <anushabaditha1999@gmail.com>
2023-12-08 14:59:11 +0530

    first commit

---
+++ cmail.py
@@ -0,0 +1,13 @@
+import smtplib
+from smtplib import SMTP
+from email.message import EmailMessage
+def sendmail(to,subject,body):
+    server=smtplib.SMTP_SSL('smtp.gmail.com',465)
+    server.login('anusha@codegnan.com','bpmebciagcubnqjs')
+    msg=EmailMessage()
+    msg['FROM']='anusha@codegnan.com'
+    msg['TO']=to
+    msg['SUBJECT']=subject
+    msg.set_content(body)
+    server.send_message(msg)
+    server.close()
